ed1f1ac0cb727719830140023dd42c9b42abae0e
Venky Venkatakrishnan <eeshwar1@gmail.com>
2021-06-12 20:40:50 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Venkateswaran Venkatakrishnan on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
